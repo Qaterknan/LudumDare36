112a8a13d17d76efe144583acf78b7addf60dba4
M4ch <MMachM@gmail.com>
2016-08-27 15:27:16 +0100

    moon
    adds a rotating satelite. SMASH!

--- code/states/mainGame.js
+++ code/states/mainGame.js
@@ -2,7 +2,8 @@ var mainGameState = new Phaser.State();
 
 mainGameState.preload = function (){
 	this.game.load.path = "assets/";
-	this.game.load.image("earth","basicEarth.png");
+	this.game.load.image("earth","planetEarth.png");
+	this.game.load.image("moon", "moon.png")
 }
 
 mainGameState.create = function (){
@@ -18,9 +19,29 @@ mainGameState.create = function (){
 	earth.width = 200;
 	
 	earth.angularSpeed = 1;
+	
+
+	
+	moon = new Phaser.Sprite(this.game, 0, 0, "moon");
+	moon.anchor.x = 0.5;
+	moon.anchor.y = 0.5;
+
+	moon.pivot.x = 0;
+	moon.pivot.y = 200;
+	
+	moon.height = 70;
+	moon.width = 70;
+
+	moon.angularSpeed = 1/14;
+	
 	earth.update = function(){
 		this.angle += this.angularSpeed;
+	} 
+
+	moon.update = function(){
+		this.angle += this.angularSpeed;
 	}
-	
+
 	earthGroup.addChild(earth);
+	this.game.world.addChild(moon);
 }
